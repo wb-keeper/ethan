2e00768a69835e53e64156f90faafd417a230750
wb-keeper <wb-keeper@mail.ru>
2023-09-15 21:00:02 +0300

    Working on about page

--- pages/about/index.js
+++ pages/about/index.js
@@ -7,7 +7,7 @@ import {
   FaWordpress,
   FaFigma,
 } from "react-icons/fa";
-
+import { motion } from "framer-motion";
 import {
   SiNextdotjs,
   SiFramer,
@@ -15,6 +15,9 @@ import {
   SiAdobephotoshop,
 } from "react-icons/si";
 import { useState } from "react";
+import Circles from "../../components/Circles";
+import Avatar from "../../components/Avatar";
+import { fadeIn } from "../../variants";
 
 //  data
 const aboutData = [
@@ -90,7 +93,52 @@ const aboutData = [
 
 const About = () => {
   const [index, setIndex] = useState(0);
-  return <div>About</div>;
+  return (
+    <div className="h-full bg-primary/30 py-32 text-center xl:text-left">
+      <Circles />
+      <motion.div
+        variants={fadeIn("right", 0.2)}
+        initial="hidden"
+        animate="show"
+        exit="hidden"
+        className="hidden xl:flex absolute bottom-0 -left-[370px] "
+      >
+        <Avatar />
+      </motion.div>
+      <div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-y-6">
+        <div>text</div>
+        <div>
+          <div className="flex gap-x-4 xl:gap-x-8 mx-auto mb-4 ">
+            {aboutData.map((item, idx) => {
+              return (
+                <div
+                  key={idx}
+                  className={` ${
+                    index === idx &&
+                    "text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300"
+                  } cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
+                  onClick={() => {
+                    setIndex(idx);
+                  }}
+                >
+                  {item.title}
+                </div>
+              );
+            })}
+          </div>
+          <div>
+            {aboutData[index].info.map((item, idx) => {
+              return (
+                <div key={idx}>
+                  <div>{item.title}</div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
