1060b9f244d7939539c372c751f7e6842893a648
wb-keeper <wb-keeper@mail.ru>
2023-09-17 19:58:29 +0300

    Ended about page and started working on services

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,4 +1,3 @@
-// icons
 import {
   FaHtml5,
   FaCss3,
@@ -18,8 +17,8 @@ import { useState } from "react";
 import Circles from "../../components/Circles";
 import Avatar from "../../components/Avatar";
 import { fadeIn } from "../../variants";
+import CountUp from "react-countup";
 
-//  data
 const aboutData = [
   {
     title: "skills",
@@ -94,7 +93,7 @@ const aboutData = [
 const About = () => {
   const [index, setIndex] = useState(0);
   return (
-    <div className="h-full bg-primary/30 py-32 text-center xl:text-left">
+    <div className="h-full bg-primary/30 py-32 text-center xl:text-left ">
       <Circles />
       <motion.div
         variants={fadeIn("right", 0.2)}
@@ -105,10 +104,84 @@ const About = () => {
       >
         <Avatar />
       </motion.div>
-      <div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-y-6">
-        <div>text</div>
-        <div>
-          <div className="flex gap-x-4 xl:gap-x-8 mx-auto mb-4 ">
+      <div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6">
+        <div className="flex-1 flex flex-col justify-center">
+          <motion.h2
+            variants={fadeIn("right", 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="h2"
+          >
+            Captivating <span className="text-accent">stories</span> birth
+            magnificent designs.
+          </motion.h2>
+          <motion.p
+            variants={fadeIn("right", 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px2 xl:px-0"
+          >
+            10 years ago, I began freelancing as a developer. Since then, I've
+            done remote work for agencies, consulted for startups, and
+            collaborated on digital products for business and consumer use.
+          </motion.p>
+          <motion.div
+            variants={fadeIn("right", 0.6)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="hidden md:flex md:max-w-xl xl:max-w-none mx-auto xl:mx-0 mb-8"
+          >
+            <div className="flex flex-1 xl:gap-x-6">
+              <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
+                <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
+                  <CountUp end={10} start={0} duration={5} /> +
+                </div>
+                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
+                  Years of experience
+                </div>
+              </div>
+              <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
+                <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
+                  <CountUp end={250} start={0} duration={5} /> +
+                </div>
+                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
+                  Satisfied clients
+                </div>
+              </div>
+              <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
+                <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
+                  <CountUp end={650} start={0} duration={5} /> +
+                </div>
+                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
+                  Finished projects
+                </div>
+              </div>
+
+              {/*!!*/}
+              <div className="relative flex-1 ">
+                <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
+                  <CountUp end={8} start={0} duration={5} /> +
+                </div>
+                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
+                  Finished projects
+                </div>
+              </div>
+
+              {/*!!*/}
+            </div>
+          </motion.div>
+        </div>
+        <motion.div
+          variants={fadeIn("left", 0.4)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          className="flex flex-col w-full xl:max-w-[48%] h-[480px]"
+        >
+          <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4 ">
             {aboutData.map((item, idx) => {
               return (
                 <div
@@ -126,16 +199,26 @@ const About = () => {
               );
             })}
           </div>
-          <div>
+          <div className=" py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start">
             {aboutData[index].info.map((item, idx) => {
               return (
-                <div key={idx}>
-                  <div>{item.title}</div>
+                <div
+                  key={idx}
+                  className="flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60"
+                >
+                  <div className="font-light mb-2 md:mb-0">{item.title}</div>
+                  <div className="hidden md:flex">-</div>
+                  <div>{item.stage}</div>
+                  <div className="flex gap-x-4">
+                    {item.icons?.map((el, idx) => {
+                      return <div className="text-2xl text-white">{el}</div>;
+                    })}
+                  </div>
                 </div>
               );
             })}
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
--- pages/services/index.js
+++ pages/services/index.js
@@ -1,5 +1,30 @@
+import { motion } from "framer-motion";
+import Circles from "../../components/Circles";
+import Bulb from "../../components/Bulb";
+import ServiceSlider from "../../components/ServiceSlider";
+
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <div className="h-full bg-primary/30 py-36 flex items-center">
+      <Circles />
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-x-8">
+          <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
+            <h2 className="h2 xl:mt-8">
+              My services <span className="text-accent">.</span>
+            </h2>
+            <p className="mb-4 max-w-[400px] mx-auto lg:mx-0 ">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
+              animi est eveniet facere impedit itaque iusto odio omnis
+              perspiciatis qui ratione repellendus.
+            </p>
+          </div>
+          <ServiceSlider />
+        </div>
+      </div>
+      <Bulb />
+    </div>
+  );
 };
 
 export default Services;
